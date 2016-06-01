af5148621f032987eb4d34239096abfe14b269cd
andrepolischuk <andre.polischuk@gmail.com>
2016-06-01 11:46:51 +0300

    Add eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import isPointerInside from 'is-pointer-inside';
 import offset from 'global-offset';
+import isPointerInside from 'is-pointer-inside';
 
-export default function isPointerNear(el, {pageX, pageY}, dist = 50) {
-  const {left, top} = offset(el);
-  const {offsetWidth, offsetHeight} = el;
+export default function isPointerNear(el, { pageX, pageY }, dist = 50) {
+  const { left, top } = offset(el);
+  const { offsetWidth, offsetHeight } = el;
   const right = left + offsetWidth;
   const bottom = top + offsetHeight;
 
-  return !isPointerInside(el, {pageX, pageY}) &&
+  return !isPointerInside(el, { pageX, pageY }) &&
     pageX >= left - dist && pageX <= right + dist &&
     pageY >= top - dist && pageY <= bottom + dist;
-};
+}
